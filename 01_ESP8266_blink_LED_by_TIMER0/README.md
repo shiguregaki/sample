9c7e5743b85c491686c5c8c8eca667728431567a
shiguregaki <shiguregaki@ysktec.com>
2019-05-12 12:05:51 +0900

    [sample1][add] Add code to blink LED by TIMER0 interrupt
    Blinking LED by TIMER0 interruput in ESP8266.

---
+++ 01_ESP8266_blink_LED_by_TIMER0/README.md
@@ -0,0 +1,2 @@
+# ESP8266_blink_LED_by_TIMER0
+Blinking LED by TIMER0 interruput in ESP8266.
---
+++ 01_ESP8266_blink_LED_by_TIMER0/src/ESP8266_blink_LED_by_TIMER0/ESP8266_blink_LED_by_TIMER0.ino
@@ -0,0 +1,32 @@
+
+/* Set these parameters to your environment. */
+#define CPU_MHZ 80
+#define blinkInterval 1 //set interval time to blink LED (seconds)
+#define ledPin 5 //set LED signal output pin on the arduino
+
+
+bool ledState = false;
+
+void led_ISR (void) {
+  if (!ledState) {
+    digitalWrite(ledPin, HIGH);
+    ledState = true;
+  } else {
+    digitalWrite(ledPin, LOW);
+    ledState = false;
+  }
+  timer0_write(ESP.getCycleCount() + 1000000L * blinkInterval * CPU_MHZ);
+  Serial.println("led_ISR");
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  noInterrupts();
+  timer0_isr_init();
+  timer0_attachInterrupt(led_ISR);
+  timer0_write(ESP.getCycleCount() + 1000000L * blinkInterval * CPU_MHZ);
+  interrupts();
+}
+
+void loop() {}
